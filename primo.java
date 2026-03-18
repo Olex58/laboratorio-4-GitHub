b130750192f6daa41e00c28baaead9c5eae4f179
daniel97 <dani.nicotina@gmail.com>
2026-03-17 22:20:26 -0400

    Desarrollo programa primo, comandos y lista2

--- primo.java
+++ primo.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 
-public class primo 
-{
+public class primo {
     public static boolean esPrimo(int n) {
-        if (n <= 1) 
+        if (n <= 1)
             return false;
 
-        if (n == 2) 
+        if (n == 2)
             return true;
 
-        if (n % 2 == 0) 
+        if (n % 2 == 0)
             return false;
         int limite = (int) Math.sqrt(n);
-        for (int i = 3; i <= limite; i += 2) 
-        {
-            if (n % i == 0) 
-                return false;           
+        for (int i = 3; i <= limite; i += 2) {
+            if (n % i == 0)
+                return false;
         }
         return true;
     }
