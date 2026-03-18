0f4f1f78cda1b222c66c9fe29d292e7ba2aab9db
alex <kratosmario5@gmail.com>
2026-03-17 21:18:41 -0400

    Primer commit

---
+++ primo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class primo {
+    public static boolean esPrimo(int n) {
+        if (n <= 1) 
+            return false;
+
+        if (n == 2) 
+            return true;
+
+        if (n % 2 == 0) 
+            return false;
+        int limite = (int) Math.sqrt(n);
+        for (int i = 3; i <= limite; i += 2) 
+        {
+            if (n % i == 0) 
+                return false;           
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+
+        if (esPrimo(numero)) {
+            System.out.println(numero + " es primo.");
+        } else {
+            System.out.println(numero + " no es primo.");
+        }
+    }
+}
